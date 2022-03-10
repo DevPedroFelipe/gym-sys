a7fef7917e813d34c8ff7bc17df10f21ce034bc7
DevPedroFelipe <tipedrofelipe@gmail.com>
2022-03-10 09:07:55 -0300

    Commit atualização da sessao.php utilizada pelo login

--- sessao.php
+++ sessao.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true)) {
+if(!isset ($_SESSION['login']) and !isset ($_SESSION['senha'])) {
     
     unset($_SESSION['login']);
     unset($_SESSION['senha']);
