0ce5a5d5d06225cc90549864a9bc038e6954744a
DevPedroFelipe <tipedrofelipe@gmail.com>
2022-03-10 15:50:06 -0300

    Implementação da funcionalidade de exclusão de usuário

---
+++ view/excluirUsuario2.php
@@ -0,0 +1,26 @@
+<?php
+
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    
+    require '../sessao.php';
+    
+}
+
+use src\RepositorioUsuario;
+
+require_once '../src/RepositorioUsuario.php';
+
+$idUsuario = $_GET['idUsuario'];
+
+$RU = new RepositorioUsuario();
+$retorno = $RU->excluirUsuario($idUsuario);
+
+if ($retorno) {
+	header("location:../index.php?cod=4&c=1");
+
+} else {
+    header("location:../index.php?cod=10&idUsuario=".$idUsuario."&c=2");
+}
+
+
+?>
