4186f7dc7af563ed9bdd56672edf48955ce50ada
DevPedroFelipe <tipedrofelipe@gmail.com>
2022-03-11 09:52:54 -0300

    Alteração da descrição do modal de confirmação de exclusão

--- view/excluirUsuario.php
+++ view/excluirUsuario.php
@@ -79,21 +79,21 @@ $Usuario = $RU->consultarUsuario($idUsuario);
             <a href="index.php?cod=4" class="btn btn-secondary">Voltar</a>
         </div>
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Confirmação de Exclusão</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Tem certeza que quer excluir o usuário(a) <strong><?= $Usuario->getNome(); ?></strong>?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <a href="view/excluirUsuario2.php?idUsuario=<?= $Usuario->getId(); ?>" class="btn btn-danger">Confirmar</a>
+                </div>
+                </div>
             </div>
-            <div class="modal-body">
-                Tem certeza que quer excluir o usuário(a) <strong><?= $Usuario->getNome(); ?></strong>?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                <a href="view/excluirUsuario2.php?idUsuario=<?= $Usuario->getId(); ?>" class="btn btn-danger">Confirmar</a>
-            </div>
-            </div>
-        </div>
         </div>
     </form>
 </div>
